2a0c4f1f237d3da6b37b150eb191360922909fb5
diptu <diptunazmulalam@gmail.com>
2024-10-30 00:13:01 +0600

    practice React:Day01

---
+++ EcoLens/src/App.jsx
@@ -0,0 +1,19 @@
+// import './App.css'
+
+function App() {
+ 
+const todos = ['Practice React','Practice Next','Create Projects','Practice ML']
+  return (
+    <>
+      <h2>Todo</h2>
+        <ul>
+          <li>{todos[0]}</li>
+          <li>{todos[1]}</li>
+          <li>{todos[2]}</li>
+          <li>{todos[3]}</li>
+        </ul>
+    </>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 365-days-of-web-dev
 A year-long journey of daily web development learning.A daily commitment to learning and practicing web development.
+
+## Day 01 : Create a React App with Vite, JSX and JS Expression
